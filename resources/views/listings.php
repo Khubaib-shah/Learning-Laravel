515a41a8722a9241aadc8b8d2bec1581e645934c
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-01 21:50:58 +0500

    Hwo we add multi lines comments

--- resources/views/listings.php
+++ resources/views/listings.php
@@ -1,9 +1,14 @@
-<h1><?php echo $heading; ?></h1>
+<!-- This is is the reguler expression of listing.php  -->
+<!--
+<h1> <?php echo $heading; ?></h1>
+
 <?php foreach ($listings as $listing): ?>
-    
     <h2><?php echo $listing['id']; ?></h2>
     <h2><?php echo $listing['title']; ?></h2>
     <p><?php echo $listing['description']; ?></p>
-
-<?php endforeach; ?> 
+<?php endforeach; ?>
+-->
 <!-- This displays the listings data that was fetched in the controller (web.php route). -->
+
+
+
