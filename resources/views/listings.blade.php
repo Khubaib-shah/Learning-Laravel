73a511c219cb38bfde8356dd0f3b4a35c4df9a08
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-01 22:05:26 +0500

    How we Use @if @else and @unless To conditinaly execute our code

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -15,14 +15,44 @@
 
 <!-- This is is the blade expression of listing.blade.php  -->
 {{-- This syntex is much cleaner --}}
-<h1> {{ $heading }}</h1>
+
+{{-- <h1> {{ $heading }}</h1>
 
 @foreach ($listings as $listing): 
     <h2>{{ $listing['id'] }}</h2>
     <h2>{{ $listing['title'] }}</h2>
     <p>{{ $listing['description'] }}</p>
-@endforeach; 
+@endforeach;  --}}
+
 <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
+{{-- How if and else work --}}
 
+{{-- This is the first way of doing if --}}
+{{-- @if (count($listings) != 0 )
+
+<h1> {{ $heading }}</h1>
+
+@foreach ($listings as $listing); 
+    <h2>{{ $listing['id'] }}</h2>
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@endforeach;
+@endif --}}
+
+
+{{-- This is the second way of doing if statement --}}
+@unless(count($listings) == 0 )
+
+<h1> {{ $heading }}</h1>
+
+@foreach ($listings as $listing);
+    <h2>{{ $listing['id'] }}</h2>
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@endforeach;
+
+@else 
+<p>No listing found</p>
+@endunless
