8958b2545596e4707b154bee3577b46b04dd5e93
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-01 21:44:33 +0500

    listings data that was fetched in the controller

---
+++ resources/views/listings.php
@@ -0,0 +1,9 @@
+<h1><?php echo $heading; ?></h1>
+<?php foreach ($listings as $listing): ?>
+    
+    <h2><?php echo $listing['id']; ?></h2>
+    <h2><?php echo $listing['title']; ?></h2>
+    <p><?php echo $listing['description']; ?></p>
+
+<?php endforeach; ?> 
+<!-- This displays the listings data that was fetched in the controller (web.php route). -->
