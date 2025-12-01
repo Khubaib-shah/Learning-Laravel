af96d85e611480949af8e316a11ed49484d330ab
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-01 20:28:44 +0500

    Learned How dynamic id Work

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/response', function () {
     return response( "<h1>Hello response</h1>" 
 )->header('Content-Type', 'text/plain')->header('foo' , 'bar');
 });
+
+
+Route::get('posts/{_id}' , function($_id){
+    return response("Post ". $_id);
+
+})->where('_id' , '[0-9]+');
