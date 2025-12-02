2259b7ca5c269e3a219ce07b45fda2fcbcd9c4c4
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-02 18:50:50 +0500

    Created the  dynamic details page of listings

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -3,17 +3,18 @@
 @section('content')
 
     @include('partials._hero')
+    @include('partials._search')
 
     <!-- This is is the reguler expression of listing.php  -->
     <!--
-                                                                                            <h1> <?php echo $heading; ?></h1>
+                                                                                                <h1> <?php echo $heading; ?></h1>
 
-                                                                                            <?php foreach ($listings as $listing): ?>
-                                                                                                <h2><?php    echo $listing['id']; ?></h2>
-                                                                                                <h2><?php    echo $listing['title']; ?></h2>
-                                                                                                <p><?php    echo $listing['description']; ?></p>
-                                                                                            <?php endforeach; ?>
-                                                                                            -->
+                                                                                                <?php foreach ($listings as $listing): ?>
+                                                                                                    <h2><?php    echo $listing['id']; ?></h2>
+                                                                                                    <h2><?php    echo $listing['title']; ?></h2>
+                                                                                                    <p><?php    echo $listing['description']; ?></p>
+                                                                                                <?php endforeach; ?>
+                                                                                                -->
     <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
