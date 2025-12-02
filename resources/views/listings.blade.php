db576d30617376a3da43ea0eaa0000b20775ccca
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-02 17:12:15 +0500

    this is how we use layout just like components

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,59 +1,66 @@
-<!-- This is is the reguler expression of listing.php  -->
-<!--
-<h1> <?php echo $heading; ?></h1>
+@extends("layout")
 
-<?php foreach ($listings as $listing): ?>
-    <h2><?php    echo $listing['id']; ?></h2>
-    <h2><?php    echo $listing['title']; ?></h2>
-    <p><?php    echo $listing['description']; ?></p>
-<?php endforeach; ?>
--->
-<!-- This displays the listings data that was fetched in the controller (web.php route). -->
+@section('content')
 
+    <!-- This is is the reguler expression of listing.php  -->
+    <!--
+    <h1> <?php echo $heading; ?></h1>
 
-
-
-<!-- This is is the blade expression of listing.blade.php  -->
-{{-- This syntex is much cleaner --}}
-
-{{-- <h1> {{ $heading }}</h1>
-
-@foreach ($listings as $listing):
-<h2>{{ $listing['id'] }}</h2>
-<h2>{{ $listing['title'] }}</h2>
-<p>{{ $listing['description'] }}</p>
-@endforeach; --}}
-
-<!-- This displays the listings data that was fetched in the controller (web.php route). -->
+    <?php foreach ($listings as $listing): ?>
+        <h2><?php    echo $listing['id']; ?></h2>
+        <h2><?php    echo $listing['title']; ?></h2>
+        <p><?php    echo $listing['description']; ?></p>
+    <?php endforeach; ?>
+    -->
+    <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
-{{-- How if and else work --}}
-
-{{-- This is the first way of doing if --}}
-{{-- @if (count($listings) != 0 )
-
-<h1> {{ $heading }}</h1>
-
-@foreach ($listings as $listing);
-<h2>{{ $listing['id'] }}</h2>
-<h2>{{ $listing['title'] }}</h2>
-<p>{{ $listing['description'] }}</p>
-@endforeach;
-@endif --}}
 
 
-{{-- This is the second way of doing if statement --}}
-<h1> {{ $heading }}</h1>
+    <!-- This is is the blade expression of listing.blade.php  -->
+    {{-- This syntex is much cleaner --}}
 
-@unless(count($listings) == 0)
+    {{-- <h1> {{ $heading }}</h1>
+
+    @foreach ($listings as $listing):
+    <h2>{{ $listing['id'] }}</h2>
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+    @endforeach; --}}
+
+    <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
-    @foreach ($listings as $listing)
-        <h2><a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a></h2>
-        <h4>{{ $listing['tags'] }}</h2>
-            <p>{{ $listing['description'] }}</p>
-    @endforeach
+    {{-- How if and else work --}}
 
-@else
-        <p>No listing found</p>
-    @endunless
+    {{-- This is the first way of doing if --}}
+    {{-- @if (count($listings) != 0 )
+
+    <h1> {{ $heading }}</h1>
+
+    @foreach ($listings as $listing);
+    <h2>{{ $listing['id'] }}</h2>
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+    @endforeach;
+    @endif --}}
+
+
+    {{-- This is the second way of doing if statement --}}
+    <h1> {{ $heading }}</h1>
+
+    @unless(count($listings) == 0)
+
+
+        @foreach ($listings as $listing)
+            <h2><a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a></h2>
+            <h4>{{ $listing['tags'] }}</h2>
+                <p>{{ $listing['description'] }}</p>
+        @endforeach
+
+    @else
+            <p>No listing found</p>
+        @endunless
+
+
+@endsection
