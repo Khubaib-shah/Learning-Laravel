e6e97da0e3aa9d23b2ca535c1b63cb3e60d3f0d1
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-02 18:29:42 +0500

    Created a layout and home page with listing components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,9 @@
-<h2>{{ $listings['id'] }}</h2>
-<h2>{{ $listings['title'] }}</h2>
-<p>{{ $listings['description'] }}</p>
+@extends("layout")
+
+@section('content')
+
+    <h2>{{ $listings['title'] }}</h2>
+    <p>{{ $listings['description'] }}</p>
+
+
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 
+    @include('partials._hero')
+
     <!-- This is is the reguler expression of listing.php  -->
     <!--
-    <h1> <?php echo $heading; ?></h1>
+                                                                                            <h1> <?php echo $heading; ?></h1>
 
-    <?php foreach ($listings as $listing): ?>
-        <h2><?php    echo $listing['id']; ?></h2>
-        <h2><?php    echo $listing['title']; ?></h2>
-        <p><?php    echo $listing['description']; ?></p>
-    <?php endforeach; ?>
-    -->
+                                                                                            <?php foreach ($listings as $listing): ?>
+                                                                                                <h2><?php    echo $listing['id']; ?></h2>
+                                                                                                <h2><?php    echo $listing['title']; ?></h2>
+                                                                                                <p><?php    echo $listing['description']; ?></p>
+                                                                                            <?php endforeach; ?>
+                                                                                            -->
     <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
@@ -47,20 +49,45 @@
 
 
     {{-- This is the second way of doing if statement --}}
-    <h1> {{ $heading }}</h1>
 
-    @unless(count($listings) == 0)
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @unless(count($listings) == 0)
 
 
-        @foreach ($listings as $listing)
-            <h2><a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a></h2>
-            <h4>{{ $listing['tags'] }}</h2>
-                <p>{{ $listing['description'] }}</p>
-        @endforeach
+            @foreach ($listings as $listing)<div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="/listings/{{ $listing->id }}">{{ $listing->title}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+                            <ul class="flex">
+                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Laravel</a>
+                                </li>
+                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">API</a>
+                                </li>
+                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
 
-    @else
+        @else
             <p>No listing found</p>
         @endunless
 
+    </div>
 
 @endsection
