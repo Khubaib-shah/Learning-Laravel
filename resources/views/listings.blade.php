96b973203523b2876f6bc764a1027ec1b59abb60
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-02 19:21:50 +0500

    This is how we create a component and pass props to it

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,14 +7,14 @@
 
     <!-- This is is the reguler expression of listing.php  -->
     <!--
-                                                                                                    <h1> <?php echo $heading; ?></h1>
+                                                                                                            <h1> <?php echo $heading; ?></h1>
 
-                                                                                                    <?php foreach ($listings as $listing): ?>
-                                                                                                        <h2><?php    echo $listing['id']; ?></h2>
-                                                                                                        <h2><?php    echo $listing['title']; ?></h2>
-                                                                                                        <p><?php    echo $listing['description']; ?></p>
-                                                                                                    <?php endforeach; ?>
-                                                                                                    -->
+                                                                                                            <?php foreach ($listings as $listing): ?>
+                                                                                                                <h2><?php    echo $listing['id']; ?></h2>
+                                                                                                                <h2><?php    echo $listing['title']; ?></h2>
+                                                                                                                <p><?php    echo $listing['description']; ?></p>
+                                                                                                            <?php endforeach; ?>
+                                                                                                            -->
     <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
@@ -55,34 +55,9 @@
         @unless(count($listings) == 0)
 
 
-            @foreach ($listings as $listing)<div class="bg-gray-50 border border-gray-200 rounded p-6">
-                    <div class="flex">
-                        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
-                        <div>
-                            <h3 class="text-2xl">
-                                <a href="/listing/{{ $listing->id }}">{{ $listing->title}}</a>
-                            </h3>
-                            <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
-                            <ul class="flex">
-                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">Laravel</a>
-                                </li>
-                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">API</a>
-                                </li>
-                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">Vue</a>
-                                </li>
-                            </ul>
-                            <div class="text-lg mt-4">
-                                <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            @foreach ($listings as $listing)
+
+                <x-listing-card :listing="$listing" />
             @endforeach
 
         @else
