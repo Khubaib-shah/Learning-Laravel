d7f36add8a818d0ac9dbf2ee6c99dd29aff4f0af
Khubaib-shah <bewithbaacha@gmail.com>
2025-12-02 19:28:29 +0500

    This is how we create a component and pass childrens same as React

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,14 +7,14 @@
 
     <!-- This is is the reguler expression of listing.php  -->
     <!--
-                                                                                                            <h1> <?php echo $heading; ?></h1>
+                                                                                                                    <h1> <?php echo $heading; ?></h1>
 
-                                                                                                            <?php foreach ($listings as $listing): ?>
-                                                                                                                <h2><?php    echo $listing['id']; ?></h2>
-                                                                                                                <h2><?php    echo $listing['title']; ?></h2>
-                                                                                                                <p><?php    echo $listing['description']; ?></p>
-                                                                                                            <?php endforeach; ?>
-                                                                                                            -->
+                                                                                                                    <?php foreach ($listings as $listing): ?>
+                                                                                                                        <h2><?php    echo $listing['id']; ?></h2>
+                                                                                                                        <h2><?php    echo $listing['title']; ?></h2>
+                                                                                                                        <p><?php    echo $listing['description']; ?></p>
+                                                                                                                    <?php endforeach; ?>
+                                                                                                                    -->
     <!-- This displays the listings data that was fetched in the controller (web.php route). -->
 
 
@@ -56,7 +56,6 @@
 
 
             @foreach ($listings as $listing)
-
                 <x-listing-card :listing="$listing" />
             @endforeach
 
